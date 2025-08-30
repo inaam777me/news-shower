c99e4635a8e9c3f84f4c49a263663a163e110e93
inaam <inaam@DESKTOP-VPEJCHG>
2025-08-31 04:15:50 +0530

    simple corrections of articales

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,11 @@ const Home = () => {
   return (
     <>
       <Navbar />
-      <ArticlesList />
+      <div className='m-4'>
+        <h1 className="text-4xl font-bold mb-4">Latest Articles</h1>
+        <ArticlesList />
+      </div>
+      
       <About />
     </>
   );
